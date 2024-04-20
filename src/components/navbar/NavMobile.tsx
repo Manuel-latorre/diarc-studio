b5f966e79331e7c0a2189ccfac9955788279596c
Manuel-latorre <manuel.latorre11@gmail.com>
2024-04-20 18:58:03 -0300

    add key in cards an changue links in navMobile

--- src/components/navbar/NavMobile.tsx
+++ src/components/navbar/NavMobile.tsx
@@ -13,32 +13,59 @@ const navLinks = [
     {path: '/#aboutus', text: 'About us'},
 ]
 
+const menuItems = [
+  "¿Qué es Tualo?",
+  "¿Cómo funciona?",
+  "¿Quines somos?",
+  "Beneficios",
+  "Blog",
+  "Preguntas frecuentes",
+];
+
 export const NavMobile = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
       <Navbar
         className="lg:hidden bg-transparent w-full fixed h-[120px]"
+        isMenuOpen={isMenuOpen}
         onMenuOpenChange={setIsMenuOpen}
       >
-        <NavbarContent>
-          <NavbarMenuToggle
-            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-            className="lg:hidden bg-transparent"
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="lg:hidden bg-transparent"
 
-          />
+        />
         </NavbarContent>
-        <NavbarMenu className={`bg-transparent z-50 mt-10`}>
-            {navLinks.map((link, index) => (
+        <NavbarMenu className={`bg-gradient-to-t from-zinc-500 to-zinc-900 bg-transparent z-50 mt-10`}>
+            {/* {navLinks.map((link, index) => (
               <NavbarMenuItem key={`${link.path + index}`}>
-                    <ActiveLinks
-                      onClick={() => setIsMenuOpen(false)}
-                      key={link.path}
-                      text={link.text}
-                      path={link.path}
-                    />
+                    <ActiveLinks path={link.path} text={link.text} onClick={() => setIsMenuOpen(false)}/>
               </NavbarMenuItem>
-              ))}
+              ))} */}
+              <div className="flex flex-col gap-4 mt-10">
+                <NavbarMenuItem>
+                  <Link onClick={() => setIsMenuOpen(false)} href={"/#solutions"} className="uppercase font-bold">
+                    Solutions
+                  </Link>
+                </NavbarMenuItem>
+                <NavbarMenuItem>
+                  <Link onClick={() => setIsMenuOpen(false)} href={"/#portfolio"} className="uppercase font-bold">
+                    Portfolio
+                  </Link>
+                </NavbarMenuItem>
+                <NavbarMenuItem>
+                  <Link onClick={() => setIsMenuOpen(false)} href={"/#aboutus"} className="uppercase font-bold">
+                    About us
+                  </Link>
+                </NavbarMenuItem>
+                <NavbarMenuItem>
+                  <Link onClick={() => setIsMenuOpen(false)} href={"/#contact"} className="uppercase font-bold">
+                    Contact
+                  </Link>
+                </NavbarMenuItem>
+              </div>
         </NavbarMenu>
       </Navbar>
     );
