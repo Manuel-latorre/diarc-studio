173833fbc37862ce272bf5bae9ed898fcbccd6c5
Manuel-latorre <manuel.latorre11@gmail.com>
2024-05-06 14:32:16 -0300

    edit text in about us

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -11,8 +11,8 @@ export default function AboutUs() {
             <div id="aboutus" className="flex flex-col items-center justify-center py-14 section">
             <div className="w-[100%] h-[100%]">
                 <div className="px-5">
-                    <p className="text-center font-semibold text-2xl translate-y-20 max-lg:text-xl max-sm:text-base">We are convinced that a multidisciplinary team is one of the keys to developing successful projects.</p>
-                    <p className="text-center font-light text-xl translate-y-28 max-lg:text-lg max-sm:text-sm max-md:translate-y-24">3D Artists <span className="px-4">|</span> Architects <span className="px-4">|</span> Designers <span className="px-4">|</span> Game Developers</p>
+                    <p className="text-center font-semibold text-2xl translate-y-20 max-lg:text-xl max-sm:text-sm">We are convinced that a multidisciplinary team is one of the keys to developing successful projects.</p>
+                    <p className="text-center font-light text-xl translate-y-28 max-lg:text-lg max-sm:text-sm max-md:translate-y-24">3D Artists <span className="md:px-4">|</span> Architects <span className="md:px-4">|</span> Designers <span className="md:px-4">|</span> Game Developers</p>
                 </div>
                 <Image className="w-[100%] h-[100%] aboutusImg" src={aboutus} alt="Team image"/>     
                 <Mask/>   
