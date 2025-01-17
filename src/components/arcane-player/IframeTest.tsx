84179811b91c9536020f02d5b0a7a657869e7fbd
Manuel-latorre <manuel.latorre11@gmail.com>
2025-01-16 21:17:28 -0300

    change video

--- src/components/arcane-player/IframeTest.tsx
+++ src/components/arcane-player/IframeTest.tsx
@@ -82,7 +82,7 @@ const VideoToIframe = () => {
             muted
             playsInline
           >
-            <source src="/test1.mp4" type="video/mp4" />
+            <source src="/test.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
           <div 
